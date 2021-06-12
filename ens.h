82e852707029b2761f94aba5020717c2b7976546
lucyrpedro <lu.ufmg@gmail.com>
2021-06-13 00:07:38 +0100

    new

---
+++ ens.h
@@ -0,0 +1,21 @@
+netcdf ce262a_pc_2012092721-2012092723 {
+dimensions:
+	axis_nbounds = 2 ;
+	lon = 96 ;
+	lat = 72 ;
+	ensemble = 2 ;
+	time_counter = UNLIMITED ; // (1 currently)
+variables:
+	float lat(lat) ;
+	float lon(lon) ;
+	double time_instant(time_counter) ;
+	double time_instant_bounds(time_counter, axis_nbounds) ;
+	double time_counter(time_counter) ;
+	double time_counter_bounds(time_counter, axis_nbounds) ;
+	double time_centered(time_counter) ;
+	double time_centered_bounds(time_counter, axis_nbounds) ;
+	double m01s00i024(time_counter, ensemble, lat, lon) ;
+	double m01s00i024_2(time_counter, ensemble, lat, lon) ;
+	double m01s00i024_3(time_counter, ensemble, lat, lon) ;
+	double m01s00i024_4(time_counter, ensemble, lat, lon) ;
+}
