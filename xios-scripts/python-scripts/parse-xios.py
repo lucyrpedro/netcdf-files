172f11bec4d1868b340fd4e8d324c2837af0652a
lucyrpedro <lu.ufmg@gmail.com>
2021-10-17 19:32:16 +0100

    files

---
+++ xios-scripts/python-scripts/parse-xios.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+import re
+import csv
+
+# Create the output filename
+
+filename = "results.csv"
+
+# Open the output file
+
+fd = open(filename, "w")
+fields = ["TIME", "INITIAL", "DUMPCTL"]
+out = csv.DictWriter(fd, fieldnames=fields, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+out.writeheader()
+
+data_M = {}
+
+# Parse the data for the information inside the result file
+
+f = open("time.txt", "r")
+
+for l in f:
+    
+    print(l)
+
+    m1 = re.match("(.*) 0 Elapsed Wallclock Time: (?P<TIME>[0-9.]*) ", l)
+    m2 = re.match("(.*) INITIAL 1 (?P<INITIAL>[0-9.]+) ", l)
+    m3 = re.match("(.*) DUMPCTL 1 (?P<DUMPCTL>[0-9.]+) ", l)
+
+    if m1:    
+        data_M.update(m1.groupdict())        
+        out.writerow(data_M)
+    if m2:
+        data_M.update(m2.groupdict())
+        out.writerow(data_M)
+    if m3:
+        data_M.update(m3.groupdict())
+        out.writerow(data_M)
+
+f.close()
+
+fd.close()
+
--- xios-scripts/res.sh
+++ xios-scripts/res.sh
@@ -13,7 +13,25 @@ cd ${aux}/results
 while IFS= read -r line
 do
 	mkdir -p $line
-	cp ~/cylc-run/${line}/work/19880901T0000Z/atmos_ens0/pe_output/*pe.stdout $line 2>/dev/null
-	cp ~/cylc-run/${line}/log/job/19880901T0000Z/atmos_main/01/job.status $line 2>/dev/null
+	cp ~/cylc-run/${line}/work/19880901T0000Z/atmos_ens0/pe_output/*pe.stdout ${line} 2>/dev/null
+	cp ~/cylc-run/${line}/log/job/19880901T0000Z/atmos_main/01/job.status ${line} 2>/dev/null
+	filename=$(ls ${line} |grep pe)
+	echo $line
+	echo $filename
 done < "$input"
 
+aux2=~/netcdf-files/xios-scripts/results
+
+while IFS= read -r line
+do
+	filename=$(ls ${aux2}/${line} |grep pe)
+        echo "line=$line"
+        echo "file=$filename"
+	cd ${aux2}/${line}
+	if [[ -s "${filename}" ]]; then
+		echo "aux=${filename}"
+		echo $(cat ${filename} |grep "Elapsed Wallclock Time") > time.txt		
+		echo $(cat ${filename} |grep " INITIAL ") >> time.txt
+		echo $(cat ${filename} |grep "DUMPCTL") >> time.txt
+	fi
+done < "$input"
