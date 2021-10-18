8d04dd6a851fb33e392164c45fac9be2bd16d55a
lucyrpedro <lu.ufmg@gmail.com>
2021-10-18 01:28:24 +0100

    files

--- xios-scripts/res.sh
+++ xios-scripts/res.sh
@@ -15,7 +15,7 @@ while IFS= read -r line
 do
 	mkdir -p $line
 	cp ~/cylc-run/${line}/work/19880901T0000Z/atmos_ens0/pe_output/*pe.stdout ${line} 2>/dev/null
-	cp ~/cylc-run/${line}/log/job/19880901T0000Z/atmos_main/01/job.status ${line} 2>/dev/null
+	cp ~/cylc-run/${line}/log/job/19880901T0000Z/atmos_main/NN/job.status ${line} 2>/dev/null
 	filename=$(ls ${line} |grep pe)
 	echo $line
 	echo $filename
