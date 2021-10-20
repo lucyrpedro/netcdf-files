756d4a97f8b5dceb9a0ce46cf3440ece4f37ae9f
lucyrpedro <lu.ufmg@gmail.com>
2021-10-20 21:23:52 +0100

    files

---
+++ script/create-stats-xml-all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Files created inside ./xml-stats-field.sh
+
+rm -rf um-stats-all.xml
+rm -rf grids.txt
+rm -rf fields.txt
+rm -rf grids-fields.txt
+
+sed 's/\"//g' um-atmos-field_ens_def.xml > aux.txt
+
+input="aux.txt"
+
+while IFS= read -r line
+do
+#	echo "Line:\n"
+	line=$(echo "$line" | grep "grid_ref")
+	sub=$(echo "$line" | cut -d'=' -f 3)
+	grid=$(echo "$sub" | cut -d' ' -f 1)
+#	echo $grid
+        sub=$(echo "$line" | cut -d'=' -f 4)
+	field=$(echo "$sub" | cut -d' ' -f 1)
+#        echo $field
+	if [ -n "$grid" ]; then
+		echo "Grid=$grid "
+		echo "Field=$field "
+		./xml-stats-field-all.sh $grid $field
+	fi
+done < "$input"
+
+rm -rf aux.txt
--- script/create-stats-xml-dim.sh
+++ script/create-stats-xml-dim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-input="grids-fields-2D.txt"
-output="um-stats-2D.xml"
+input="grids-fields.txt"
+output="um-stats-all.xml"
 rm -rf $output
 
 while IFS= read -r line
@@ -10,19 +10,6 @@ do
 	field=$(echo "$line" | cut -d' ' -f 2)
 	echo "grid = $grid"
 	echo "field = $field"
-	./xml-stats-field-dim.sh $grid $field $output
-done < "$input"
-
-input="grids-fields-3D.txt"
-output="um-stats-3D.xml"
-rm -rf $output
-
-while IFS= read -r line
-do
-        grid=$(echo "$line" | cut -d' ' -f 1)
-        field=$(echo "$line" | cut -d' ' -f 2)
-        echo "grid = $grid"
-        echo "field = $field"
-        ./xml-stats-field-dim.sh $grid $field $output
+	./xml-stats-field-all.sh $grid $field $output
 done < "$input"
 
