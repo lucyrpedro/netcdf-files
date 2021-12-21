7d896d6854116f23b701c96a752ffc000b04a122
lucyrpedro <lu.ufmg@gmail.com>
2021-12-21 22:39:22 +0000

    files

--- xios-scripts/res3.sh
+++ xios-scripts/res3.sh
@@ -14,6 +14,7 @@ cd ${aux}/results
 while IFS= read -r line
 do
 	mkdir -p $line
+        cp ~/cylc-run/${line}/work/19880901T0000Z/atmos_main/pe_output/*pe.stdout ${line} 2>/dev/null
 	cp ~/cylc-run/${line}/work/19880901T0000Z/atmos_ens0/pe_output/*pe.stdout ${line} 2>/dev/null
 	cp ~/cylc-run/${line}/work/19880901T0000Z/atmos_ens0/xios_client_000.out ${line} 2>/dev/null
 	cp ~/cylc-run/${line}/log/job/19880901T0000Z/atmos_main/NN/job.status ${line} 2>/dev/null
