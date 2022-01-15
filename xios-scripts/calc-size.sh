7c38938e418c2c44785c960d08803a5eee9bf50d
lucyrpedro <lu.ufmg@gmail.com>
2022-01-15 22:50:29 +0000

    files

---
+++ xios-scripts/calc-size.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./size.sh u-ch427-n96-max18
+
+./size.sh u-ch427-n96-max18-mf
+
+./size.sh u-ch427-n96-max18-single
+
--- xios-scripts/size.sh
+++ xios-scripts/size.sh
@@ -3,54 +3,51 @@
 iter=18
 curr="/home/n02/n02/lrpedro/test-git/netcdf-files/xios-scripts"
 
-ls -l |grep test | cut -d' ' -f 5,9 > t1
-
-dir1="u-ch427-n96-max18-ens"
-dir2="u-ch427-n96-max18-mf-ens"
-dir3="u-ch427-n96-max18-single-ens"
+suite=$1
 
 for (( c=0; c<=$iter; c++ ))
 do
 	if [[ $c -lt 10 ]]
 	then
-		dir4="${dir1}0$c"
-		echo "$dir4"
-		cd /home/n02/n02/lrpedro/cylc-run/$dir4/work/19880901T0000Z/atmos_main
+		ens="${suite}-ens0$c"
+		echo "$ens"
+		cd /home/n02/n02/lrpedro/cylc-run/$ens/work/19880901T0000Z/atmos_main
 		ls -l |grep test | cut -d' ' -f 5,9 > t1
 	else
-		dir4="$dir1$c"
-		echo "$dir4"
+		ens="$suite-ens$c"
+		echo "$ens"
 	fi
         
-	dir="/home/n02/n02/lrpedro/cylc-run/${dir4}/work/19880901T0000Z/atmos_main/"	
+	dir="/home/n02/n02/lrpedro/cylc-run/${ens}/work/19880901T0000Z/atmos_main/"	
 	echo $dir
 	cd $dir
-	echo -e "Suite, Netcdf File, Size, Size (bytes)" > aux-${dir4}
-	echo $dir4 > aux1
+	echo -e "Suite, Netcdf File, Size, Size (bytes)" > aux-${ens}
+	echo $ens > aux1
 	ls -lh |grep test | cut -c 43-61 > aux2
 	ls -lh |grep test | cut -c 25-30 > aux3
 	ls -l |grep test | cut -c 25-35 > aux4
-	paste -d ',' aux1 aux2 aux3 aux4 >> aux-${dir4}
+	paste -d ',' aux1 aux2 aux3 aux4 >> aux-${ens}
 
 	total=0
 	while IFS= read -r line
 	do
-		total=$(echo "$total + $line" |bc)
+#		total=$(echo "$total + $line" |bc)
+		total=$(echo $total $line |awk '{print $1 + $2}')
 #		echo $line
 	done < aux4
-	echo -e "$dir4,,Total Size,$total\n" >> aux-${dir4}
+	echo -e "$ens,,Total Size,$total\n" >> aux-${ens}
 
 	rm aux1 aux2 aux3 aux4
-	cp aux-$dir4 aux-$c
-	mv aux-$dir4 $curr
+	cp aux-$ens aux-$c
+	mv aux-$ens $curr
 	mv aux-$c $curr
 done
 
 cd $curr
-rm -rf size.csv
-touch size.csv
+rm -rf size-$suite.csv
+touch size-$suite.csv
 
 for (( c=0; c<=$iter; c++ ))
 do
-	cat aux-$c >> size.csv
+	cat aux-$c >> size-$suite.csv
 done
