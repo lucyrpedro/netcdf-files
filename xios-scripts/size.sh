ac487ce2be6cd1575eb0bfa14783692c195893ec
lucyrpedro <lu.ufmg@gmail.com>
2022-01-15 22:28:25 +0000

    files

--- xios-scripts/size.sh
+++ xios-scripts/size.sh
@@ -9,7 +9,7 @@ dir1="u-ch427-n96-max18-ens"
 dir2="u-ch427-n96-max18-mf-ens"
 dir3="u-ch427-n96-max18-single-ens"
 
-for (( c=1; c<=$iter; c++ ))
+for (( c=0; c<=$iter; c++ ))
 do
 	if [[ $c -lt 10 ]]
 	then
@@ -19,27 +19,43 @@ do
 		ls -l |grep test | cut -d' ' -f 5,9 > t1
 	else
 		dir4="$dir1$c"
-		echo "$dir1"
+		echo "$dir4"
 	fi
         
 	dir="/home/n02/n02/lrpedro/cylc-run/${dir4}/work/19880901T0000Z/atmos_main/"	
 	echo $dir
 	cd $dir
-	echo $dir4 > aux0
-	ls -lh |grep test | cut -c 43-61 > aux1
-	ls -lh |grep test | cut -c 25-30 > aux2
-	ls -l |grep test | cut -c 25-35 > aux3
-	paste -d ',' aux0 aux1 aux2 aux3 > aux-${dir4}
-	rm aux0 aux1 aux2 aux3
+	echo -e "Suite, Netcdf File, Size, Size (bytes)\n" > aux-${dir4}
+	echo -e "\n" >> aux-${dir4}
+	echo $dir4 > aux1
+	ls -lh |grep test | cut -c 43-61 > aux2
+	ls -lh |grep test | cut -c 25-30 > aux3
+	ls -l |grep test | cut -c 25-35 > aux4
+	paste -d ',' aux1 aux2 aux3 aux4 >> aux-${dir4}
+#	echo " " >> aux-${dir4}
+
+	nfiles=$(ls |grep test |wc -l)
+
+	total=0
+	while IFS= read -r line
+	do
+		total=$(echo "$total + $line" |bc)
+		echo $line
+	done < aux4
+#	echo $total
+	echo -e "$dir4,,Total,$total\n" >> aux-${dir4}
+
+	rm aux1 aux2 aux3 aux4
 	cp aux-$dir4 aux-$c
 	mv aux-$dir4 $curr
 	mv aux-$c $curr
 done
 
 cd $curr
-touch file
+rm -rf file
+touch file.csv
 
-for (( c=1; c<=$iter; c++ ))
+for (( c=0; c<=$iter; c++ ))
 do
-	cat aux-$c >> file
+	cat aux-$c >> file.csv
 done
