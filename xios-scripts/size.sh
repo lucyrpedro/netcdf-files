352f9a76f8e0138b716fbba4f30b2a970697c26f
lucyrpedro <lu.ufmg@gmail.com>
2022-01-15 22:32:13 +0000

    files

--- xios-scripts/size.sh
+++ xios-scripts/size.sh
@@ -25,25 +25,20 @@ do
 	dir="/home/n02/n02/lrpedro/cylc-run/${dir4}/work/19880901T0000Z/atmos_main/"	
 	echo $dir
 	cd $dir
-	echo -e "Suite, Netcdf File, Size, Size (bytes)\n" > aux-${dir4}
-	echo -e "\n" >> aux-${dir4}
+	echo -e "Suite, Netcdf File, Size, Size (bytes)" > aux-${dir4}
 	echo $dir4 > aux1
 	ls -lh |grep test | cut -c 43-61 > aux2
 	ls -lh |grep test | cut -c 25-30 > aux3
 	ls -l |grep test | cut -c 25-35 > aux4
 	paste -d ',' aux1 aux2 aux3 aux4 >> aux-${dir4}
-#	echo " " >> aux-${dir4}
-
-	nfiles=$(ls |grep test |wc -l)
 
 	total=0
 	while IFS= read -r line
 	do
 		total=$(echo "$total + $line" |bc)
-		echo $line
+#		echo $line
 	done < aux4
-#	echo $total
-	echo -e "$dir4,,Total,$total\n" >> aux-${dir4}
+	echo -e "$dir4,,Total Size,$total\n" >> aux-${dir4}
 
 	rm aux1 aux2 aux3 aux4
 	cp aux-$dir4 aux-$c
