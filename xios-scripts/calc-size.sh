f3a3cd464d7cfd46f852daf81312926fa53d10b6
lucyrpedro <lu.ufmg@gmail.com>
2022-01-23 00:36:32 +0000

    files

--- xios-scripts/calc-size.sh
+++ xios-scripts/calc-size.sh
@@ -5,6 +5,7 @@
 ./size.sh u-ch427-n96-max18
 ./size.sh u-ch427-n96-max18-mf
 ./size.sh u-ch427-n96-max18-single
+./size.sh u-ch427-n96-max18-stdev
 ./size.sh u-ch427-n216-max18
 ./size.sh u-ch427-n216-max18-mf
 ./size.sh u-ch427-n216-max18-single
@@ -26,7 +27,10 @@ touch total-all-suites.csv
 	suite=u-ch427-n96-max18-single
         cat total-$suite.csv >> total-all-suites.csv
         cat blank.txt >> total-all-suites.csv
-        suite=u-ch427-n216-max18
+        suite=u-ch427-n96-max18-stdev
+        cat total-$suite.csv >> total-all-suites.csv
+        cat blank.txt >> total-all-suites.csv
+	suite=u-ch427-n216-max18
         cat total-$suite.csv >> total-all-suites.csv
         cat blank.txt >> total-all-suites.csv
         suite=u-ch427-n216-max18-mf
