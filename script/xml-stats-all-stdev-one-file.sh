1abb0320fb8d5b0f1fbfea99c07a2ec606eccb0f
lucyrpedro <lu.ufmg@gmail.com>
2022-02-03 04:06:47 +0000

    files

---
+++ script/xml-stats-all-stdev-one-file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+grid=$1
+field=$2
+file=$3
+
+cat >> ${file} << EOF
+
+		<field
+			id="${field}_stdev_file"
+			name="${field}_stdev_file"
+			operation="instant"
+			field_ref="${field}_stdev"
+		/>
+EOF
+
