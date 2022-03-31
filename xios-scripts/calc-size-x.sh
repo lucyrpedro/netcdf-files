409d7c953bb84344a75d2f614814e335fe7a4caa
lucyrpedro <lu.ufmg@gmail.com>
2022-03-31 22:32:09 +0100

    files

--- xios-scripts/calc-size-x.sh
+++ xios-scripts/calc-size-x.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# suites=2
-
-./size-x.sh u-ch427-n1280-x
-./size-x.sh u-ch427-n1280-x5
-./size-x.sh u-ch427-n1280-x-orig
-./size-x.sh u-ch427-n1280-x5-orig
-./size-x.sh u-ch427-n1280-x-ms
-./size-x.sh u-ch427-n1280-x5-ms
 ./size-pack.sh u-ch427-n1280-pack0
 ./size-pack.sh u-ch427-n1280-pack5
 ./size-pack.sh u-ch427-n1280-start-paper-pack0
@@ -16,32 +8,17 @@
 rm -rf total-all-suites-x.csv
 touch total-all-suites-x.csv
 
-#for (( c=0; c<=$suites; c++ ))
-#do
-	suite=u-ch427-n1280-x
-	cat total-$suite.csv >> total-all-suites.csv
-	cat blank.txt >> total-all-suites.csv
-	suite=u-ch427-n1280-x5
-        cat total-$suite.csv >> total-all-suites.csv
-        cat blank.txt >> total-all-suites.csv
-        suite=u-ch427-n1280-x-orig
-        cat total-$suite.csv >> total-all-suites.csv
-        cat blank.txt >> total-all-suites.csv
-        suite=u-ch427-n1280-x5-orig
-        cat total-$suite.csv >> total-all-suites.csv
-        cat blank.txt >> total-all-suites.csv
-        suite=u-ch427-n1280-x-ms
-        cat total-$suite.csv >> total-all-suites.csv
-        cat blank.txt >> total-all-suites.csv
-        suite=u-ch427-n1280-x5-ms
-        cat total-$suite.csv >> total-all-suites.csv
-        cat blank.txt >> total-all-suites.csv	
         suite=u-ch427-n1280-pack0
         cat total-$suite.csv >> total-all-suites.csv
         cat blank.txt >> total-all-suites.csv
         suite=u-ch427-n1280-pack5
         cat total-$suite.csv >> total-all-suites.csv
         cat blank.txt >> total-all-suites.csv
-#done
+        suite=u-ch427-n1280-start-paper-pack0
+        cat total-$suite.csv >> total-all-suites.csv
+        cat blank.txt >> total-all-suites.csv
+        suite=u-ch427-n1280-start-paper-pack5
+        cat total-$suite.csv >> total-all-suites.csv
+        cat blank.txt >> total-all-suites.csv
 
 
