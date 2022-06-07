a22a0baae97a8156db82910e1dcc4b44ff6c2b90
lucyrpedro <lu.ufmg@gmail.com>
2022-06-07 14:25:21 +0100

    files

--- temp/temp-stat/plot-temp.R
+++ temp/temp-stat/plot-temp.R
@@ -32,19 +32,19 @@ x = seq(1, x2, 1)
 
 
 plot(x,temp_inst[1:27648],type="l",col="blue")
-plot(x,temp_inst[27649:55296],type="l",col="blue")
-lines(x,temp_max,col="green")
-lines(x,temp_min,col="red")
-lines(x,temp_mean,col="orange")
+plot(x,temp_inst[27649:55296],type="l",col="pink")
+lines(x,temp_max,type="l",col="green",)
+lines(x,temp_min,type="l",col="red")
+lines(x,temp_mean,type="l",col="orange")
 #lines(x,temp_stdev,col="pink")
-plot_colors <- c("blue","green","red","orange", "pink")
+plot_colors <- c("blue","pink","green","red","orange")
 legend(x = "top",inset = 0,
-        legend = c("Instant", "Max", "Min","Mean", "Stdev"), 
+        legend = c("ens0", "ens1","max", "min","mean"), 
         col=plot_colors, lwd=5, cex=.5, horiz = TRUE)
 
 
-plot(x,temp_inst[1:27648],type="l",col="blue")
-lines(x,temp_inst[27649:55296],type="l",col="blue")
+plot(x,temp_inst[1:27648],col="blue")
+lines(x,temp_inst[27649:55296],col="pink")
 
 plot(x,temp_max,col="green")
 
