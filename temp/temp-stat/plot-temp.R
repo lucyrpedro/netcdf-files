8748eb7b083b5bf784658fbe90e90c1685c0bb3b
lucyrpedro <lu.ufmg@gmail.com>
2022-06-07 14:17:08 +0100

    files

---
+++ temp/temp-stat/plot-temp.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+# ########################################################
+
+pdf("temp.pdf") # either save all files in one pdf or the files in specific pdfs; find an option to automatise the choice
+
+# temp_inst = read.csv("temp-inst.csv")
+# temp_max = read.csv("temp-max.csv")
+# temp_min = read.csv("temp-min.csv")
+# temp_mean = read.csv("temp-mean.csv")
+
+# data = read.table("test.csv", header=FALSE, sep = ',')
+temp_inst = read.table("inst2.txt", header=FALSE, sep = ',')
+temp_max = read.table("max2.txt", header=FALSE, sep = ',')
+temp_min = read.table("min2.txt", header=FALSE, sep = ',')
+temp_mean = read.table("mean2.txt", header=FALSE, sep = ',')
+temp_stdev = read.table("stdev2.txt", header=FALSE, sep = ',')
+
+x1=length(temp_inst)
+x2=length(temp_max)
+x3=length(temp_min)
+x4=length(temp_mean)
+x5=length(temp_stdev)
+
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
+
+l1 = seq(1, x1, 1)
+l2 = seq(1, x2, 1)
+
+
+plot(l2,temp_inst[1:27648],type="l",col="blue")
+plot(l2,temp_inst[27649:55296],type="l",col="blue")
+lines(l2,temp_max,col="green")
+lines(l2,temp_min,col="red")
+lines(l2,temp_mean,col="orange")
+lines(l2,temp_stdev,col="pink")
+plot_colors <- c("blue","green","red","orange", "pink")
+legend(x = "top",inset = 0,
+        legend = c("Instant", "Max", "Min","Mean", "Stdev"), 
+        col=plot_colors, lwd=5, cex=.5, horiz = TRUE)
+
+
+#plot(x,temp_inst,type="l",col="blue")
+#plot(x,temp_max,col="green")
+#plot(x,temp_min,col="red")
+#plot(x,temp_mean,col="yellow")
+#plot(x,temp_stdev,col="magenta")
+
+# dev.off()
+print("Graphics constructed with success!")
