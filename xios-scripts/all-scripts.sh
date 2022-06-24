e6798cccf6f3ba4b72878708cf70759314bfcf48
lucyrpedro <lu.ufmg@gmail.com>
2022-06-24 21:29:59 +0100

    files

--- xios-scripts/all-scripts.sh
+++ xios-scripts/all-scripts.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-./res3.sh
-./res-1280.sh
-./parse-xios-all3.py
-./calc-size.sh
-./calc-size-x.sh
+./res4.sh
+./parse-xios-all5.py
+./parse-xios-took2.py
+./calc-size-all.sh
 
 git add -A; git commit -m "files"; git push;
 
